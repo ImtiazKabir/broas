f257900f3ebe4e8372ee0b4d5ce244dc10121c08
ImtiazKabir <imtiazkabir.imtiaz@gmail.com>
2024-10-03 23:27:02 +0600

    fixed isa; added lexer.h

---
+++ lexer.h
@@ -0,0 +1,18 @@
+#ifndef LEXER_H_
+#define LEXER_H_
+
+#define MX_TOK_SZ 32
+
+enum TokenType { LABEL, OPCODE, REGISTER, IMMEDIATE };
+
+struct LexToken {
+  enum TokenType type;
+  union {
+    int tokint;
+    char tokstr[MX_TOK_SZ];
+  } token;
+};
+
+int getTokens(int fd, struct LexToken *tokens);
+
+#endif /* !LEXER_H_ */
